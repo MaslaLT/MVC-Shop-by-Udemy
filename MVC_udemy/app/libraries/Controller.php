5010c26129c1a2be96efad5d6a50e6a9d909a071
MaslaLT <artur.masel@gmail.com>
2018-09-05 15:06:56 +0300

    class Controller added method model and methon view
    In app/libraries/Controller.php
    
    class Pages extended Controller
    
    In app/Controllers/Pages.php

--- MVC_udemy/app/libraries/Controller.php
+++ MVC_udemy/app/libraries/Controller.php
@@ -1,8 +1,40 @@
 <?php
 /**
+ * Base Controller
+ * Loads the models and view
+ *
  * Created by PhpStorm.
  * User: lazeris
  * Date: 31/08/2018
  * Time: 10:02
  */
 
+class Controller
+{
+    /**
+     * @param $model
+     * @return mixed
+     */
+    public function model($model)
+    {
+        $modelUrl = '../app/models/'. $model. '.php';
+        require_once ($modelUrl);
+
+        return new $model();
+    }
+
+    /**
+     * @param $view
+     * @param array $data
+     */
+    public function view($view, $data = [])
+    {
+        $viewUrl = '../app/views/'. $view. '.php';
+
+        if(file_exists($viewUrl)){
+            require_once ($viewUrl);
+        } else {
+            die('View dos not exists');
+        }
+    }
+}
