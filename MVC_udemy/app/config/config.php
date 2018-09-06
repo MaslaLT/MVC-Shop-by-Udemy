5934e41d85431f3e5681de6a2ba3add8e9478520
MaslaLT <artur.masel@gmail.com>
2018-09-06 12:58:33 +0300

    Added global variables(APPROOT, URLROOT, SITENAME).
    In app/config/config.php

---
+++ MVC_udemy/app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lazeris
+ * Date: 06/09/2018
+ * Time: 12:43
+ */
+
+// App root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// Url root
+define('URLROOT', 'http://localhost');
+
+//Site Name
+define('SITENAME', 'SharePost');
