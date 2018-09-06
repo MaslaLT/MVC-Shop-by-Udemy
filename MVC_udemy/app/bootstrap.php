70f7a4a4dd3f1143189d66493d40badd8c1bc8c0
MaslaLT <artur.masel@gmail.com>
2018-09-06 14:24:47 +0300

    removed var_dump

--- MVC_udemy/app/bootstrap.php
+++ MVC_udemy/app/bootstrap.php
@@ -5,5 +5,4 @@ require_once 'config/config.php';
 // Auto load core libraries
 spl_autoload_register(function($classNeme){
     require_once 'libraries/' . $classNeme . '.php';
-    var_dump($classNeme);
 });
